ec1e99af682799d007a989382266084b6c4504d4
idifro <idifro@protonmail.com>
2019-03-16 10:05:07 +0530

    Initial

---
+++ Level 1 Calculator.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    float n1,n2,r;
+    char op;
+    int flag=0;
+    printf("Welcome to Calculator\n");
+    printf("Enter the first number\n");
+    scanf("%f",&n1);
+    printf("Enter the second number\n");
+    scanf("%f",&n2);
+    printf("Enter the operation to be performed\n");
+    printf("The list of operations available are +,-,*,/\n");
+    scanf(" %c",&op);
+    switch(op)
+    {
+        case '+':
+            r = n1+n2;
+            break;
+        case'-':
+            r = n1-n2;
+            break;
+        case '*':
+            r = n1*n2;
+            break;
+        case '/':
+            r = n1/n2;
+            if(n2==0 || n1==0){
+                flag = 1;
+            }
+            break;
+        default:
+            printf("Invalid input\n");
+    }
+    if(flag){
+        printf("Invalid input or operation\n");
+    }
+    printf("The answer is %.3f",r);
+}
